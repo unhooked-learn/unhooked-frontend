59c735c15d1eaacec2de22ef635598cfb432cd7b
Kevin Wrede <kevin.wrede@student.htw-berlin.de>
2020-05-25 15:51:30 +0200

    #64 / knowledgebase chapter view layout fix

--- pages/knowledge/_chapter/index.vue
+++ pages/knowledge/_chapter/index.vue
@@ -1,29 +1,41 @@
 <template>
-    <div class="flex items-center justify-center min-h-screen px-4 py-12 bg-gray-50 sm:px-6 lg:px-8">
-        <div class="w-full max-w-md">
-            <div class="absolute top-5 left-5 ">
-                <nuxt-link :to="localePath('knowledge')"><font-awesome-icon icon="chevron-left"/></nuxt-link>
-            </div>  
-            <div>
-            <div class="object-cover aspect aspect-1/2">
-                <img :src="movie.Poster" class="object-cover min-w-full mt-4 overflow-hidden rounded-md max-h-40" >
-            </div>  
-            <h2 class="mt-4 text-2xl font-extrabold leading-9 text-center text-gray-900">
-                {{ movie.Title }}
-            </h2>
-            </div>
-            <h3 class="mt-4 mb-4 ml-1 text-lg font-medium text-gray-900" >
-                {{ movie.Plot }}
-            </h3>  
-        </div>
-    </div>   
+  <div
+    class="flex items-center justify-center px-4 py-8 bg-gray-50 sm:px-6 lg:px-8"
+  >
+    <div class="w-full max-w-md">  
+      <div class="absolute top-0 right-0 object-cover min-w-full aspect aspect-1/2">
+          <img
+            :src="movie.Poster"
+            class="object-cover overflow-hidden rounded-b-md max-h-40"
+          />
+      </div>
+
+      <div class="absolute top-0 left-0 text-white">
+        <nuxt-link :to="localePath('knowledge')">
+          <font-awesome-icon icon="chevron-left" class="m-5" />
+          </nuxt-link>
+      </div>
+      <div>
+        <h2
+          class="text-2xl font-extrabold leading-9 text-center text-gray-900 mt-36"
+        >
+          {{ movie.Title }}
+        </h2>
+      </div>
+      <h3 class="mt-4 mb-4 text-lg font-medium text-gray-900">
+        {{ movie.Plot }}
+      </h3>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   layout: 'clear',
-  async asyncData ({ params, $axios }) {
-    const data = await $axios.$get(`http://www.omdbapi.com/?i=${params.chapter}&apikey=466f9280&plot=full`)
+  async asyncData({ params, $axios }) {
+    const data = await $axios.$get(
+      `http://www.omdbapi.com/?i=${params.chapter}&apikey=466f9280&plot=full`
+    )
     return { movie: data }
   }
 }
