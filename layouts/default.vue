0ce4748c938a684b24a8457dfbf037514555ef20
Philipp Wahle <phil.wahle@student.htw-berlin.de>
2020-05-25 23:18:32 +0200

    Bugfix/64 knowledge chapter layout fixes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,17 @@
 <template>
-  <div class="h-screen font-sans antialiased bg-gray-50">
-    <div class="relative max-w-md mx-auto">
-      <nuxt />
-      <UHNavigation />
-      <client-only>
-        <UHModal />
-      </client-only>
-    </div>
-  </div>
+  <Master>
+    <UHNavigation />
+  </Master>  
 </template>
 
 <script>
 import UHNavigation from '@/components/layout/UHNavigation'
-import UHModal from '@/components/generics/UHModal'
+import Master from './master'
 
 export default {
   components: {
     UHNavigation,
-    UHModal
+    Master
   }
 }
 </script>
