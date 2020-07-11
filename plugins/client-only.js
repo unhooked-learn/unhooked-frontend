020312206c24bc529342bc8117dc46a3b775558c
Kevin Wrede <kevin.wrede@student.htw-berlin.de>
2020-07-11 21:56:24 +0200

    #158 vue-scrollto added, scrolling to answers id after clicking check answer button

--- plugins/client-only.js
+++ plugins/client-only.js
@@ -6,9 +6,11 @@ import { dom } from '@fortawesome/fontawesome-svg-core';
 import VueObserveVisibility from 'vue-observe-visibility'
 import VueFaker from 'vue-faker'
 import StarRating from 'vue-star-rating'
-
-Vue.component('star-rating', StarRating)
+import VueScrollTo from 'vue-scrollto'
 import VuePullRefresh from 'vue-pull-refresh'
+
+Vue.use(VueScrollTo)
+Vue.component('star-rating', StarRating)
 Vue.component('vue-pull-refresh', VuePullRefresh)
 Vue.use(VueFaker);
 Vue.use(VueObserveVisibility)
